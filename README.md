5463fbd3d97fd93a5e13e9b991aa0f3bb80f19b6
whilenot-dev <me@whilenot.dev>
2022-04-10 22:52:38 +0200

    Add code and README

---
+++ README.md
@@ -0,0 +1,17 @@
+# Steaming video via Motion JPEG and counting established TCP sockets
+
+The source code accompanying the [blog post]().
+
+## Usage
+
+To launch the MJPEG server:
+
+```bash
+$ ./src/mjpeg-server/serve
+```
+
+To launch the SPA:
+
+```bash
+$ ./src/spa-server/serve
+```
---
+++ src/spa-server/main.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+  main();
+});
+
+function main() {
+  addStream();
+  window.setTimeout(removeStream, 10000);
+}
+
+function addStream() {
+  const img = document.createElement("img");
+
+  img.setAttribute("id", "stream");
+  img.setAttribute("alt", "loading stream...");
+  img.setAttribute("src", "http://0.0.0.0:8001/"); // ref to the netcat MJPEG server above
+  img.setAttribute("height", "480");
+  img.setAttribute("width", "640");
+
+  let count = 0;
+  img.addEventListener("load", () => {
+    console.log(`images loaded: ${++count}`);
+  });
+
+  document.body.appendChild(img);
+}
+
+function removeStream() {
+  const img = document.getElementById("stream");
+  if (!img) {
+    return;
+  }
+
+  // without the removal of the `src` attribute, the stream will continue
+  // loading data in the background
+  img.removeAttribute("src");
+
+  img.remove();
+}
